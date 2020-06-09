7c8ad02e64006280ccfbc784d76fc538d1396615
Cillié Malan <cilliemalan@gmail.com>
2020-06-09 11:39:56 +0200

    Split functionality into multiple files

---
+++ src/strings.ts
@@ -0,0 +1,140 @@
+import {
+    ISA, ABI, ObjectType,
+    ProgramHeaderEntryType, SectionHeaderEntryType,
+    SymbolType, SymbolBinding, SymbolVisibility
+} from './types';
+
+export function ISAToString(isa: ISA): string {
+    switch (isa) {
+        case ISA.IA64: return 'IA-64';
+        case ISA.RiscV: return 'RISC-V';
+        default:
+            return ISA[isa] || isa.toString();
+    }
+}
+
+export function ABIToString(abi: ABI): string {
+    switch (abi) {
+        case ABI.GNUHurd: return 'GNU Hurd';
+        case ABI.NovelloModesto: return 'Novello Modesto';
+        case ABI.HPUX: return 'HP-UX';
+        case ABI.NonStopKernel: return 'NonStop Kernel';
+        case ABI.FenixOS: return 'Fenix OS';
+        default:
+            return ABI[abi] || abi.toString();
+    }
+}
+
+export function ObjectTypeToString(objectType: ObjectType): string {
+    return ObjectType[objectType] || objectType.toString();
+}
+
+export function ProgramHeaderEntryTypeToString(programHeaderEntryType: ProgramHeaderEntryType): string {
+    switch (programHeaderEntryType) {
+        case ProgramHeaderEntryType.ProgramHeaderTable:
+            return 'Program Header Table';
+        default:
+            return ProgramHeaderEntryType[programHeaderEntryType] || programHeaderEntryType.toString();
+    }
+}
+
+export function SectionHeaderEntryTypeToString(sectionHeaderEntryType: SectionHeaderEntryType): string {
+    switch (sectionHeaderEntryType) {
+        case SectionHeaderEntryType.Null: return 'NULL section';
+        case SectionHeaderEntryType.ProgBits: return 'Prog bits';
+        case SectionHeaderEntryType.SymTab: return 'Symbol table';
+        case SectionHeaderEntryType.StrTab: return 'String table';
+        case SectionHeaderEntryType.Rela: return 'Relocation section with addends';
+        case SectionHeaderEntryType.Hash: return 'Symbol hash table';
+        case SectionHeaderEntryType.Dynamic: return 'Dynamic';
+        case SectionHeaderEntryType.Note: return 'Note';
+        case SectionHeaderEntryType.NoBits: return 'No bits';
+        case SectionHeaderEntryType.Rel: return 'Relocation section';
+        case SectionHeaderEntryType.ShLib: return 'ShLib';
+        case SectionHeaderEntryType.DynSym: return 'Dynamic linking symbols section';
+        case SectionHeaderEntryType.InitArray: return 'Init array';
+        case SectionHeaderEntryType.FiniArray: return 'Fini array';
+        case SectionHeaderEntryType.PreInitArray: return 'Pre-init array';
+        case SectionHeaderEntryType.Group: return 'Section group';
+        case SectionHeaderEntryType.ShNdx: return 'Extended symbol table section index';
+        case SectionHeaderEntryType.Num: return 'Num section';
+        case SectionHeaderEntryType.GnuAttributes: return 'GNU object attributes';
+        case SectionHeaderEntryType.GnuHash: return 'GNU hash section';
+        case SectionHeaderEntryType.GnuLibList: return 'GNU pre-link library list';
+        case SectionHeaderEntryType.GnuVerDef: return 'GNU version definition section';
+        case SectionHeaderEntryType.GnuVerNeed: return 'GNU version needs section';
+        case SectionHeaderEntryType.GnuVerSym: return 'GNU version symbol table';
+        default:
+            return SectionHeaderEntryType[sectionHeaderEntryType] || (sectionHeaderEntryType as number).toString();
+    }
+}
+
+export function sectionFlagsToString(flags: number | BigInt) {
+    if (flags instanceof BigInt) {
+        flags = Number(flags);
+    }
+
+    let str = [];
+    if (flags & 0x1) str.push('Writeable');
+    if (flags & 0x2) str.push('Alloc');
+    if (flags & 0x4) str.push('Executable');
+    if (flags & 0x10) str.push('Merge');
+    if (flags & 0x20) str.push('Strings');
+    if (flags & 0x40) str.push('Info Link');
+    if (flags & 0x80) str.push('Link Order');
+    if (flags & 0x100) str.push('Nonconforming');
+    if (flags & 0x200) str.push('Group');
+    if (flags & 0x400) str.push('Thread Local Storage');
+    if (flags & 0x4000000) str.push('Special ordering');
+    if (flags & 0x8000000) str.push('Exclude');
+    if (str.length == 0) return '<none>';
+    return str.join(' | ');
+}
+
+export function programHeaderFlagsToString(flags: number) {
+    let str = [];
+    if (flags & 0x2) str.push('Write');
+    if (flags & 0x4) str.push('Read');
+    if (flags & 0x1) str.push('Execute');
+    return str.join(' | ');
+}
+
+export function elfFlagsToString(isa: ISA, flags: number): string {
+    if (isa === ISA.ARM) {
+        const ver = ((flags & 0xff000000) >> 24);
+        let str = [
+            `Version: ${ver}`
+        ];
+        if (flags & 0x00800000) str.push('BE-8');
+        if (ver <= 4 && flags & 0x00400000) str.push('Legacy');
+        if (ver >= 5 && flags & 0x00000400) str.push('Hard Float');
+        if (ver >= 5 && flags & 0x00000200) str.push('Soft Float');
+        return str.join(' | ');
+    }
+
+    return flags.toString();
+}
+
+export function symbolTypeToString(type: SymbolType) {
+    switch (type) {
+        case SymbolType.RelocationExpression: return "Relocation Expression";
+        case SymbolType.SignedRelocationExpression: return "Signed Relocation Expression";
+        case SymbolType.ThreadLocalStorage: return "Thread Local Storage";
+        default: return SymbolType[type] || type.toString();
+    }
+}
+
+export function symbolBindingToString(binding: SymbolBinding) {
+    return SymbolBinding[binding] || binding.toString();
+}
+
+export function symbolVisibilityToString(visibility: SymbolVisibility) {
+    return SymbolVisibility[visibility] || visibility.toString();
+}
+
+export function shndxToString(shndx: number) {
+    if (shndx === 0) return 'Undefined';
+    else if (shndx === 0xfffffff1) return 'Absolute';
+    else if (shndx === 0xfffffff2) return 'Common';
+    else return shndx.toString();
+}
---
+++ src/types.ts
@@ -0,0 +1,131 @@
+
+export enum ISA {
+    None = 0x00,
+    SPARC = 0x02,
+    x86 = 0x03,
+    MIPS = 0x08,
+    PowerPC = 0x14,
+    S390 = 0x16,
+    ARM = 0x28,
+    SuperH = 0x2A,
+    IA64 = 0x32,
+    x64 = 0x3E,
+    AArch4 = 0xB7,
+    RiscV = 0xF3
+}
+
+export enum ABI {
+    SystemV = 0x00,
+    HPUX = 0x01,
+    NetBSD = 0x02,
+    Linux = 0x03,
+    GNUHurd = 0x04,
+    Solaris = 0x06,
+    AIX = 0x07,
+    IRIX = 0x08,
+    FreeBSD = 0x09,
+    Tru64 = 0x0A,
+    NovelloModesto = 0x0B,
+    OpenBSD = 0x0C,
+    OpenVMS = 0x0D,
+    NonStopKernel = 0x0E,
+    AROS = 0x0F,
+    FenixOS = 0x10,
+    CloudABI = 0x11,
+    ARM = 0x61,
+    Standalone = 0xff,
+}
+
+export enum ObjectType {
+    None = 0x00,
+    Relocatable = 0x01,
+    Executable = 0x02,
+    Shared = 0x03,
+    Core = 0x04,
+}
+
+export enum ProgramHeaderEntryType {
+    Null = 0x00,
+    Load = 0x01,
+    Dynamic = 0x02,
+    Interp = 0x03,
+    Note = 0x04,
+    ShLib = 0x05,
+    ProgramHeaderTable = 0x06
+}
+
+export enum SectionHeaderEntryType {
+    // Inactive section with undefined values
+    Null = 0,
+    // Information defined by the program, includes executable code and data
+    ProgBits = 1,
+    // Section data contains a symbol table
+    SymTab = 2,
+    // Section data contains a string table
+    StrTab = 3,
+    // Section data contains relocation entries with explicit addends
+    Rela = 4,
+    // Section data contains a symbol hash table. Must be present for dynamic linking
+    Hash = 5,
+    // Section data contains information for dynamic linking
+    Dynamic = 6,
+    // Section data contains information that marks the file in some way
+    Note = 7,
+    // Section data occupies no space in the file but otherwise resembles SHT_PROGBITS
+    NoBits = 8,
+    // Section data contains relocation entries without explicit addends
+    Rel = 9,
+    // Section is reserved but has unspecified semantics
+    ShLib = 10,
+    // Section data contains a minimal set of dynamic linking symbols
+    DynSym = 11,
+    // Section data contains an array of constructors
+    InitArray = 14,
+    // Section data contains an array of destructors
+    FiniArray = 15,
+    // Section data contains an array of pre-constructors
+    PreInitArray = 16,
+    // Section group
+    Group = 17,
+    // Extended symbol table section index
+    ShNdx = 18,
+    // Number of reserved SHT_* values
+    Num = 19,
+    // Object attributes
+    GnuAttributes = 0x6ffffff5,
+    // GNU-style hash section
+    GnuHash = 0x6ffffff6,
+    // Pre-link library list
+    GnuLibList = 0x6ffffff7,
+    // Version definition section
+    GnuVerDef = 0x6ffffffd,
+    // Version needs section
+    GnuVerNeed = 0x6ffffffe,
+    // Version symbol table
+    GnuVerSym = 0x6fffffff,
+}
+
+export enum SymbolType {
+    None = 0,
+    Object = 1,
+    Function = 2,
+    Section = 3,
+    File = 4,
+    Common = 5,
+    ThreadLocalStorage = 6,
+    RelocationExpression = 7,
+    SignedRelocationExpression = 8
+}
+
+export enum SymbolBinding {
+    Local = 0,
+    Global = 1,
+    Weak = 2
+}
+
+export enum SymbolVisibility {
+    Default = 0,
+    Internal = 1,
+    Hidden = 2,
+    Protected = 3
+}
