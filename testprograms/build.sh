9ba120a135bfa3f8ae4cde2502437c29110f44d5
Cillié Malan <cilliemalan@gmail.com>
2021-01-29 12:39:59 +0200

    More and parallel builds of stuff

--- testprograms/build.sh
+++ testprograms/build.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-make -f Makefile.gcc-x64
-make -f Makefile.clang-x64
-make -f Makefile.gcc-arm-eabi
-make -f Makefile.gcc-riscv-eabi
+make -f Makefile.gcc-x64 &
+make -f Makefile.clang-x64 &
+make -f Makefile.gcc-arm-eabi &
+make -f Makefile.gcc-riscv-eabi &
+make -f Makefile.gcc-aarch64 &
+make -f Makefile.gcc-aarch64_be &
+make -f Makefile.gcc-arm &
+
+wait
+
